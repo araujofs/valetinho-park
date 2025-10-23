b1efeda2f5c62ed712390a921cc0e1a520080ff7
Arthur <arthurfelipe5567@gmail.com>
2025-10-22 23:45:49 -0300

    fix: make apagar right

--- src/appconsole/Apagar.java
+++ src/appconsole/Apagar.java
@@ -1,5 +1,7 @@
 package appconsole;
 
+import java.util.List;
+
 import modelo.Bilhete;
 import modelo.Estacionamento;
 import modelo.Veiculo;
@@ -9,30 +11,49 @@ public class Apagar {
 
   public Apagar() {
     try {
+      System.out.println("------------------------ removendo veiculo -----------------------");
       Veiculo veic = Repositorio.lerPlaca("JKL-3456");
-
-      Bilhete bil = veic.getListaBilhete().getFirst();
-
-      Estacionamento est = bil.getEstacionamento();
-
+      List<Bilhete> bilhetes = veic.getListaBilhete();
       System.out.println("Veiculo: " + veic);
-      System.out.println("Estacionamento: " + est);
-      System.out.println("Bilhete: " + bil);
+      System.out.println();
+
+      System.out.println("------------------------ removendo bilhetes dos respectivos estacionamentos -----------------------");
+      for (Bilhete bil : bilhetes) {
+        System.out.println("bilhete atual");
+        System.out.println("Bilhete: " + bil);
+        System.out.println();
+
+        Estacionamento est = bil.getEstacionamento();
+        System.out.println("------------------ estacionamento velho ---------------------");
+        System.out.println("Estacionamento: " + est + " com bilhetes: " + est.getListaBilhete());
+        System.out.println();
+
+        est.getListaBilhete().remove(bil);
+        Repositorio.gravar(est);
+
+        est = Repositorio.lerId(est.getId(), est.getClass());
+        System.out.println("------------------ estacionamento atualizado ---------------------");
+        System.out.println("Estacionamento: " + est + " com bilhetes: " + est.getListaBilhete());
+        System.out.println("------------------------------------------------------------------");
+        System.out.println();
+      }
+
+      System.out.println();
+
       Repositorio.deletar(veic);
-      Repositorio.deletar(est);
-
-      bil.setEstacionamento(null);
       
-      System.out.println("Estacionamento: " + bil.getEstacionamento());
-      Repositorio.gravar(bil);
-      System.out.println("Bilhete: " + bil);
+      System.out.println("------------------- tentando procurar pelos veiculo e bilhetes deletados -----------------");
 
-      // fica orfão
-      Repositorio.deletar(bil);      
+      veic = Repositorio.lerPlaca("JKL-3456");
+      System.out.println("Veiculo: " + veic);
+      System.out.println();
 
-      bil = Repositorio.lerId(bil.getId(), Bilhete.class);
-
-      System.out.println("Bilhete: " + bil);
+      for (Bilhete bil : bilhetes) {
+        System.out.println("bilhete atual");
+        System.out.println("Bilhete: " + bil);
+        System.out.println("resultado: " + Repositorio.lerId(bil.getId(), bil.getClass()));
+        System.out.println();
+      }
     } catch (Exception e) {
       System.out.println("--->" + e.getMessage());
     }
